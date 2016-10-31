f9a870a2ffb0c1349a2490e2d7750e9386031de1
Tommy <foertmtc@mail.uc.edu>
2016-10-31 16:55:19 -0400

    Initial commit of dropbox test

---
+++ dropboxTest.py
@@ -0,0 +1,23 @@
+import dropbox
+import ConfigParser
+import datetime
+
+print 'Dropbox API test'
+
+ini = 'config.ini'
+config = ConfigParser.SafeConfigParser()
+config.read(ini)
+
+app_key = config.get('Dropbox','app_key')
+app_secret = config.get('Dropbox','app_secret')
+access_token = config.get('Dropbox','access_token')
+
+dbx = dropbox.Dropbox(access_token)
+dbx.users_get_current_account()
+
+time = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+
+dbx.files_upload(time,'/'+time+'.txt')
+
+for entry in dbx.files_list_folder('').entries:
+    print(entry.name)
