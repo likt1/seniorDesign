8a3a502cf23cd47858465ce51bb816100c78b724
root <root@vagrant>
2017-01-31 14:06:34 -0500

    #13 create template shell program to create / install new services, should be ported to python...

---
+++ proofOfConcept/service-creator/service-creator.sh
@@ -0,0 +1,79 @@
+#!/bin/sh
+# based off of gist.github.com/naholyr/4275302
+
+SERVICE_FILE=$(tempfile)
+TEMPLATE_FILE=template-service.sh
+
+
+echo "---validate template---"
+if [ -f "$TEMPLATE_FILE" ]
+then
+	echo "found, continue with service creation..."
+else
+	echo "template file not found, stopping service creation..."
+	exit 1
+fi
+
+echo "--- Customize new service ---"
+echo "You will now be prompted for some customizations for your new service"
+echo "Press Ctrl+C anytime to abort."
+echo "Empty values are not accepted."
+echo ""
+
+prompt_token() {
+	local VAL=""
+	while [ "$VAL" = "" ]; do
+		echo -n "${2:-$1} : "
+		read VAL
+		if [ "$VAL" = "" ]; then
+			echo "Please provide a value"
+		fi
+	done
+	VAL=$(printf '%q' "$VAL")
+	eval $1=$VAL
+	sed -i "s/<$1>/$(printf '%q' "$VAL")/g $SERVICE_FILE"
+}
+
+prompt_token 'NAME'	'Service name'
+if [ -f "/etc/init.d/$NAME" ]; then
+	echo "Error: service '$NAME' already exists"
+	exit 1
+fi
+
+prompt_token 'DESCRIPTION'	' Description'
+prompt_token 'COMMAND'		'     Command'
+prompt_token 'USERNAME'		'        User'
+if ! id -u '$USERNAME' &> /dev/null; then
+	echo "Error: user '$USERNAME' not found"
+	exit 1
+fi
+
+echo ""
+
+echo "--- Installation ---"
+if [ ! -w /etc/init.d ]; then
+	echo "I don't have nough permissions to install the service..."
+	echo "to install it manually run the following:"
+	echo ""
+	echo "	mv\"$SERVICE_FILE\" \"/etc/init.d/$NAME\""
+	echo "	touch \"/var/log/$NAME.log\" && chown \"$USERNAME\" \"/var/log/$NAME.log\""
+	echo "	update-rc.d \"$NAME\" defaults"
+	echo "	service \"$NAME\" start"
+else
+	echo "1. mv\"$SERVICE_FILE\" \"/etc/init.d/$NAME\""
+	mv -v "$SERVICE_FILE" "/etc/init.d/$NAME"
+	echo "2. touch \"/var/log/$NAME.log\" && chown \"$USERNAME\" \"/var/log/$NAME.log\""
+	touch "/var/log/$NAME.log" && chown "$USERNAME" "/var/log/$NAME.log"
+	echo "3. update-rc.d \"$NAME\" defaults"
+	update-rc.d "$NAME" defaults
+	echo "4. service \"$NAME\" start"
+	service "$NAME" start
+fi
+
+echo ""
+echo "---Uninstall Instructions---"
+echo "The service can uninstall itself:"
+echo "	service \"$NAME\" uninstall"
+echo "It will simply run update-rc.d -f \"$NAME\" remove && rm -f \"/etc/init.d/$Name\""
+echo ""
+echo "--- Done ---"
---
+++ proofOfConcept/service-creator/test-service-prgm.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <string.h>  // For string ops...
+
+// custom macros to configure log location and log content
+#define DIR_LOC "/home/vagrant/"
+#define FILE_NAME "poc-service-test.log"
+#define TO_LOG "hello\n"
+
+int main()
+{
+   FILE *fptr;
+   char fileLocation[50];
+   strcpy(fileLocation,DIR_LOC);
+   strcat(fileLocation,FILE_NAME);
+   fptr = fopen(fileLocation, "w");
+   if(fptr == NULL)
+   {
+      printf("Error while opening the output file!\n");
+      return -1;
+   }
+   
+   fprintf(fptr,"%s\n", TO_LOG);
+   
+   fclose(fptr);
+
+   return 0;
+}
