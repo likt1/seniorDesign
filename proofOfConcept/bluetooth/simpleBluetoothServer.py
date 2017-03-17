07a4780115664d6b8c9eb3a45574e308f62b450f
Tommy Foertmeyer <foertmtc@mail.uc.edu>
2017-03-17 15:22:57 -0400

    #28 Adding remote control to the server

--- proofOfConcept/bluetooth/simpleBluetoothServer.py
+++ proofOfConcept/bluetooth/simpleBluetoothServer.py
@@ -3,10 +3,12 @@ import bluetooth
 import json
 import time
 import dropbox
+import configparser as ConfigParser
 
 import pdb
 
 import wifiScript as w 
+import dioScript as d
 
 server_sock=bluetooth.BluetoothSocket( bluetooth.RFCOMM )
 
@@ -18,6 +20,9 @@ print("listening on port %d" % port)
 uuid = "00001101-0000-1000-8000-00805F9B34FB"
 bluetooth.advertise_service( server_sock, "Inspirado Service", uuid )
 
+app_key = "1mqt8n70964mn8n"
+app_secret = "3p9t99oz4i9cgg3"
+
 """
 Network Objects:
 ID                  [int]
@@ -28,38 +33,82 @@ ConnectionStatus    [string]
 """
 
 def handleCommand(cmd, parameters = ""):
-    switch={"listNetworks":listNetworks,"connectToNetwork":connectToNetwork,"default_value":defaultReply}
+    # Switch for handling various commands with associated parameters
+    switch={"listNetworks":listNetworks,
+        "connectToNetwork":connectToNetwork,
+        "getAccessKey":getAccessKey,
+        "listDIOValues":listDIOValues,
+        "accessKeyToServer":accessKeyToServer,
+        "control":handleControl,
+        "default_value":defaultReply}
     try:
         msg = switch[cmd](parameters)
     except KeyError:
         msg = switch["default_value"]()
 
+    #pdb.set_trace()
+
     client_sock.send(msg.encode('utf-8'))
       
 def listNetworks(parameters):
-    #Get json object representing available networks from wifi script
+    # Get json object representing available networks from wifi script
     network_object = w.scanNetworks()
     json_string = json.dumps([element.__dict__ for element in network_object])
     prepend = '{"type":"listNetworks","networks":'
     append = '}'
     
-    networks = prepend + json_string + append
-    return networks
+    networks_cmd = prepend + json_string + append
+    return networks_cmd
 
 def connectToNetwork(parameters):
-    # Goal: Run configure Network command with appropriate credentials from app
-    # App should send json object with type connectToNetwork as well as ssid, username if necessary, and pwd 
-    # w.configureNetwork(log_file, selected_network)
+    # Run configure Network command with appropriate credentials from app
 
-    # Hardcoded example  
-    # network_item = w.NetworkItem("2","not connected","ZyXEL24474","wifi_88532ee764bd_5a7958454c3234343734_managed_psk","wpa-psk")
-    
-    print("[!] PARAMETERS [%s]" % parameters)
-    network_item = w.NetworkItem(parameters['id'],parameters['status'], parameters['ssid'], parameters['serviceKey'], parameters['security'], parameters['username'])
+    network_item = w.NetworkItem(parameters['id'],parameters['status'], parameters['ssid'], parameters['serviceKey'], parameters['security'])
     log_file = open("log","a")
-    w.configureNetwork(log_file, network_item)
+    item = w.configureNetwork(log_file, network_item, parameters['username'],parameters['password'])
+
+    #pdb.set_trace()
+    print(item[0].__dict__)
 
     return "[!] CONNECTION ATTEMPTED"
+
+def getAccessKey(parameters):
+    # Prompt client to send its dropbox access key
+    access_key_cmd = '{"type":"getAccessKey"}'
+    return access_key_cmd
+
+def accessKeyToServer(parameters):
+    # Retrieve access key from the client
+    access_key = parameters['access_key']    
+
+    Config = ConfigParser.ConfigParser()
+    cfgfile = open("config.ini",'w')
+    Config.add_section("Dropbox")
+    Config.set('Dropbox','app_key',app_key)
+    Config.set('Dropbox','app_secret',app_secret)
+    Config.set('Dropbox','access_key',access_key)
+
+    Config.write(cfgfile)
+    cfgfile.close()
+
+    return "[!] Access key sent to server"
+
+def handleControl(parameters):
+    d.setValue(parameters['property'],parameters['value'])
+    return "[!] Handled control command"
+
+def listDIOValues(parameters):
+    # Get json object representing DIO values
+    dio_object = d.getValues() 
+    # Ex: '{"format":"wav","time":"active","toggle"="True"}'
+    json_string = dio_object.serialize()
+    prepend = '{"type":"listDIO","settings":'
+    append = '}'
+    
+    #pdb.set_trace()
+
+    dio_cmd = prepend + json_string + append
+    return dio_cmd 
 
 def defaultReply():
     return "[!] Not a valid command"
@@ -73,7 +122,11 @@ while True:
         greeting = b"\x41\x43\x4B"                  #Send ACK to acknowledge connection
         client_sock.send(greeting)
         handleCommand('listNetworks')
-        
+        time.sleep(.1)
+        handleCommand('listDIOValues')  
+        time.sleep(.1)
+        handleCommand('getAccessKey')
+            
         while True:
             data = client_sock.recv(1024)
             data = data.decode('UTF-8')
