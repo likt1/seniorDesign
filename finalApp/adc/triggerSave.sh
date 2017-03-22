1ff8f83db3e35ae1c6dc5eecec150d33526ea271
root <root@beaglebone.localdomain>
2017-03-22 06:27:54 +0000

    Add parameters for extension and raw file name

--- finalApp/adc/triggerSave.sh
+++ finalApp/adc/triggerSave.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 
+# $1 represents raw file, $2 represents compression type
+
 # create timestamp variable
 tmp=$(date)
 DATE=$(echo $tmp | sed 's/ //g') 
 
 # convert file (may need to be done in circular buffer)
-sox -r 44100 -e unsigned -b 16 -c 1 ~/$DATE.raw ~/$DATE.wav
+sox -r 44100 -e unsigned -b 16 -c 1 ~/$1 ~/$DATE.$2
 
 # sync our wav with the cloud
-python3 /root/seniorDesign/finalApp/sendToDropbox.py $DATE.wav
+python3 /root/seniorDesign/finalApp/sendToDropbox.py $DATE.$2
 
-./copyToSD /root/$DATE.raw 
-./copyToSD /root/$DATE.wav 
+#./copyToSD /root/$1 # For development testing 
+./copyToSD /root/$DATE.$2
