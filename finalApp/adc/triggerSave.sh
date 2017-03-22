770f6782102bf3100c45f247a73c5005e0ada3ef
root <root@beaglebone.localdomain>
2017-03-22 06:04:23 +0000

    Script to save with date and time

--- finalApp/adc/triggerSave.sh
+++ finalApp/adc/triggerSave.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
+# create timestamp variable
+tmp=$(date)
+DATE=$(echo $tmp | sed 's/ //g') 
+
 # convert file (may need to be done in circular buffer)
-sox -r 44100 -e unsigned -b 16 -c 1 ~/testOut.raw ~/testOut-44k1.wav
+sox -r 44100 -e unsigned -b 16 -c 1 ~/$DATE.raw ~/$DATE.wav
 
 # sync our wav with the cloud
-#python3 ../dropBox.py testOut-44k1.wav
+python3 /root/seniorDesign/finalApp/sendToDropbox.py $DATE.wav
 
-./copyToSD /root/testOut.raw 
-./copyToSD /root/testOut-44k1.wav 
+./copyToSD /root/$DATE.raw 
+./copyToSD /root/$DATE.wav 
