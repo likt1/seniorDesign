4836d3aa6112c44e095bcf19f34363606b962f57
root <root@beaglebone.localdomain>
2017-03-22 06:06:37 +0000

    Improvements to dropbox script

--- finalApp/dropBox/sendToDropbox.py
+++ finalApp/dropBox/sendToDropbox.py
@@ -2,37 +2,42 @@
 
 import sys
 import dropbox
-import configparser
+import configparser as ConfigParser
 import datetime
 
 """
 INSPIRADO
+ 
+Upload to Dropbox - Python script
 
-Proof of Concept: 
-    Upload to Dropbox - Python script
-
-Author: Thomas Foertmeyer
 """
+
+ini = '/root/conf/config.ini'
 
 def main(fileName):
     ### Load key, secret, and access token from config.ini ###
-    ini = 'config.ini'
     config = ConfigParser.SafeConfigParser()
     config.read(ini)
 
     app_key = config.get('Dropbox','app_key')
     app_secret = config.get('Dropbox','app_secret')
-    access_token = config.get('Dropbox','access_token')
+    access_token = config.get('Dropbox','access_key')
     ###
-
-    dbx = dropbox.Dropbox(access_token)
-    dbx.users_get_current_account()
+    
+    try:
+        dbx = dropbox.Dropbox(access_token)
+        dbx.users_get_current_account()
+    except Exception as e:
+        print(" Unable to connect to Dropbox")
+        sys.exit(1)
 
     try:
         with open(fileName, 'rb') as f:
             dbx.files_upload(f.read(),'/'+fileName, mute=True)
-    except Exception as err:
-        print("Failed to upload %s\n%s" % (file, err))
+        print("[!] Uploaded %s to Dropbox\n" % (fileName))
+    except Exception as e:
+        print("Failed to upload %s\n%s" % (file, e))
+        sys.exit(1)
 
 if __name__ == "__main__":
     if len(sys.argv) < 2:
