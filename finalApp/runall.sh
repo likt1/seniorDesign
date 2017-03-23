b61310cd1e40ecb1a07c88bdb5566c27f98002b7
farleykm <farleykm@mail.uc.edu>
2017-03-23 04:31:00 -0400

    #40 update runall.sh to track process ids and restore working dir

--- finalApp/runall.sh
+++ finalApp/runall.sh
@@ -1,17 +1,26 @@
-# kick off all things at once
+# kick off all things at once (store process ids into /root/)
+cwd=$(pwd)
+
 #cd /root/seniorDesign/finalApp/adc
 #make clean
 #make
 
 cd /root/seniorDesign/finalApp/adc
 nice --10 ./circularBuffer &
+echo "$!" > /root/cbufPID 
 
 cd /root/seniorDesign/finalApp/bluetooth
 python3 simpleBluetoothServer.py &
+echo "$!" > /root/btPID 
 
 cd /root/seniorDesign/finalApp/gpio
 python3 DIO.py &
+echo "$!" > /root/dioPID 
 python3 EnactPWM.py &
+echo "$!" > /root/ledPID 
+
+# restore previous working directory
+cd $cwd 
 
 # to start the services run the python3 service-creator.py
 # then run "executeProgram.sh python3 /root/seniorDesign/finalApp/gpio/EnactPWM.py"
